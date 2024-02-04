a83ceb47428bee2cdeaea6b9b026c4ee0250d53b
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-04 21:43:39 +0000

    init and writetofile modified to include filename argument

--- starter/index.js
+++ starter/index.js
@@ -1,90 +1,94 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const { title } = require("process");
 
-const util = require('util');
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
-  
-    {
-      type : "input",
-      message : "What is the title of the project",
-      name : "title"
-    },
-    {
-      type : "input",
-      message : "Describe your project",
-      name :"description"
-    },
-    {
-      type : "input",
-      message : "Please input the installation process of the application",
-      name : "installation"
-    },
-    {
-      type: "input",
-      message : "Please input the usage steps for the application",
-      name : "usage"
-    },
-    {
-      type : "checkbox",
-      message : "Please choose a License for your application",
-      name : "license",
-      choices : ["Apache License 2.0", "GNU General Public License v3.0", "MIT license", "Creative Commons Zero v1.0 Universal", "Eclipse Pulic Licese 2.0"]
-},
-{
-  type : "input",
-  message : "How do you want others to contribute towards this project. List them here",
-  name : "contribute"
-},
-{
-  type : "input",
-  message : "How can this application be tested?",
-  name : "tests"
-},
-{
-  type : "input",
-  message : "Please enter your guthub username",
-  name : "github"
-},
-{
-  type : "input",
-  message : "Please enter your email address",
-  name : "email"
-},
-{
-  type : "input",
-  message : "Add credits here",
-  name : "credits"
-},
-{
-  type : "input",
-  message : "Add resources here",
-  name : "resources"
-}
+  {
+    type: "input",
+    message: "What is the title of the project",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Describe your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please input the installation process of the application",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please input the usage steps for the application",
+    name: "usage",
+  },
+  {
+    type: "checkbox",
+    message: "Please choose a License for your application",
+    name: "license",
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT license",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Pulic Licese 2.0",
+    ],
+  },
+  {
+    type: "input",
+    message:
+      "How do you want others to contribute towards this project. List them here",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "How can this application be tested?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Please enter your guthub username",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Please enter your email address",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Add credits here",
+    name: "credits",
+  },
+  {
+    type: "input",
+    message: "Add resources here",
+    name: "resources",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-  return generateMarkdown(data)
+  return writeFileAsync(fileName, generateMarkdown(data));
 }
-
-
 
 // function to initialize program
 function init() {
-  inquirer.prompt(questions)
-  .then((data) => {
-  const markDown = writeToFile("SAMPLEREADME.md",data)
-  return writeFileAsync("SAMPLEREADME.md", markDown)})
-  .then(() => console.log("Successfully wrote to the readme file"))
-  .catch((err) => console.error(err))
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      return writeToFile("SAMPLEREADME.md", data);
+    })
+    .then(() => console.log("Successfully wrote to the readme file"))
+    .catch((err) => console.log(err));
 }
 
 // function call to initialize program
 init();
-
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -42,7 +42,7 @@ The application can be tested using ${data.tests.trim()}.
 
 ### Questions 
 
-If you would like to get in touch through Github, you follow the link below to find my github page [GitHub](https://github.com/${data.github.trim()}) 
+If you would like to get in touch through Github, [Click Here](https://github.com/${data.github.trim()}) 
 
 If you have additional questions, you can contact me through my email address : [Email](mailto:${data.email.trim()})
 
